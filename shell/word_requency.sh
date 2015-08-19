ad6e24a5413c8781a2585b84a3228e3956fbcf77
jeanpan <beats.1213@gmail.com>
2015-08-18 22:43:18 -0400

    add note

--- shell/word_requency.sh
+++ shell/word_requency.sh
@@ -1,3 +1,7 @@
 # Read from the file words.txt and output the word frequency list to stdout.
 # Word Frequency https://leetcode.com/submissions/detail/36808885/
-cat words.txt | tr -s ' ' '\n' | sort | uniq -c | awk '{print $2" "$1}' | sort -k2 -n -r
+# Note : 
+# 1. tr -s ' ' matches "one or more spaces" 
+# 2. sort -k2 means sort value by second field
+# 3. -r -n should be at the same level 
+cat words.txt | tr -s ' ' '\n' | sort | uniq -c | awk '{print $2" "$1}' | sort -k2 -rn
