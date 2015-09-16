bacdb1ac4f1721709715755982cf5607c74c5e2b
jeanpan <beats.1213@gmail.com>
2015-09-15 23:30:43 -0400

    add get function

--- algorithms/hashtable.py
+++ algorithms/hashtable.py
@@ -35,3 +35,42 @@ class HashTable:
 
     def rehash(self, oldhash, size):
         return (oldhash + 1) % size
+
+    def get(self, key):
+        startslot = self.hashfunction(key, len(self.slots))
+
+        data = None
+        stop = False
+        found = False
+        pos = startslot
+
+        while self.slots[pos] != None and \
+            not found and not stop:
+
+            if self.slots[pos] == key:
+                found = True
+                data = self.data[pos]
+            else:
+                pos = self.rehash(pos, len(self.slots))
+                if pos == startslot:
+                    stop = True
+        return data
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, data):
+        self.put(key, data)
+
+H = HashTable()
+H[54]="cat"
+H[26]="dog"
+H[93]="lion"
+H[17]="tiger"
+H[77]="bird"
+H[31]="cow"
+H[44]="goat"
+H[55]="pig"
+H[20]="chicken"
+print H.slots
+print H.data
